2d0fb2b45c4ce9469a28fd0929f0d77ebaa16eb4
coyote <megan.metelak@gmail.com>
2022-04-22 16:35:20 -0500

    executed loginUser, addUser, saveBook, removeBook mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,59 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
+// execute loginUser mutation
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+// execute addUser mutation
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+// execute saveBook mutation
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+    $bookId: String!
+    $authors: [String]!
+    $description: String!
+    $title: String!
+    $image: String!
+  ) {
+    saveBook(
+      bookId: $bookId
+      authors: $authors
+      description: $description
+      title: $title
+      image: $image
+    ) {
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+// execute removeBook mutation
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
