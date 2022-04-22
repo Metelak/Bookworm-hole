cf4c49c0210d9c568468f6ba104615a8376b3a18
coyote <megan.metelak@gmail.com>
2022-04-22 13:19:21 -0500

    mutations for creating, updating, deleting user/books

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,7 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
     // define query functionality to work with Mongoose Models
     Query: {
-      me: async (parent, args, context) => {
+      user: async (parent, args, context) => {
         if (context.user) {
           const userData = await User.findOne({ id: context.user._id})
             .select('-__v -password');
@@ -16,10 +16,66 @@ const resolvers = {
         throw new AuthenticationError('Not logged in');
       }
     },
+    // define mutation functionality to work with Mongoose Model
     Mutation: {
-        login:
-
-        saveBook:
-
-        deleteBook:
+        // create
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+      
+            return { token, user };
+        },
+        // update
+        saveBook: async(parent, args, context) => {
+            if (context.user) {
+              const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: {
+                  bookId: args.bookId,
+                  authors: args.authors,
+                  title: args.title,
+                  description: args.description,
+                  image: args.image
+                } } },
+                { new: true, runValidators: true }
+              );
+      
+              return updatedUser;
+            }
+      
+            throw new AuthenticationError('You need to be logged in');
+        },
+        // delete
+        deleteBook: async(parent, args, context) => {
+            if (context.user) {
+              const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: args.bookId } } },
+                { new: true }
+              );
+      
+              return updatedUser;
+            }
+      
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+        }
     }
+};
+    module.exports = resolvers;
