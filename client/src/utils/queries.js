7dea4f76e7d44b98852cbdd4304a16d8be37b3ce
coyote <megan.metelak@gmail.com>
2022-04-22 16:01:36 -0500

    copied over user and book typedefs for me query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,16 @@
 import { gql } from '@apollo/client';
+// me query
+export const GET_ME = gql`
+_id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+      bookId: String
+      authors: [String]
+      description: String
+      title: String
+      image: String
+      link: String
+`
+    
