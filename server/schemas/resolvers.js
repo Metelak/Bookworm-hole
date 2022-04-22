a5d98154bc3f1dcc9002b7d6f77a54ce0a02fa28
coyote <megan.metelak@gmail.com>
2022-04-22 13:04:07 -0500

    initiated defining query and mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,25 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    // define query functionality to work with Mongoose Models
+    Query: {
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ id: context.user._id})
+            .select('-__v -password');
+  
+            return userData;
+        }
+  
+        throw new AuthenticationError('Not logged in');
+      }
+    },
+    Mutation: {
+        login:
+
+        saveBook:
+
+        deleteBook:
+    }
